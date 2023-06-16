8e79a53c6c096b4b35b7c396a9156b5a98ab0d6a
Maelikah <eva.longoria@gmail.com>
2023-06-15 21:02:48 -0600

    Created folder and files structure

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        // Stored in milliseconds
+        maxAge: parseInt(process.env.SESSION_MAX_AGE),
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+        console.log(
+        `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+        )
+    );
+});
