ba2bea2119e1f3ead6ddd1f7283ff195c65115df
unknown <gergasa94@gmail.com>
2023-06-19 19:36:21 -0600

    first-commit-agenciesModel

---
+++ models/Agencies.js
@@ -0,0 +1,44 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Agencies extends Model {}
+
+Agencies.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    webpage: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'agencies',
+  }
+);
+
+module.exports = Agencies;
