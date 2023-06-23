92e1bc0090bf450aebf8d31d8bb7a665710e9852
Andrea Ceron <118624352+aceron2205@users.noreply.github.com>
2023-06-22 22:05:27 -0500

    navigate to query template through js, temporarily handle null by omitting value

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,46 +1,72 @@
-const cityDropdown = document.querySelector('#city-dropdown');
-const typeDropdown = document.querySelector('#type-dropdown');
-const breedDropdown = document.querySelector('#breed-dropdown');
-const genderDropdown = document.querySelector('#gender-dropdown');
-const ageRangeDropdown = document.querySelector('#age-range-dropdown');
-const agencyDropdown = document.querySelector('#agency-dropdown');
-const searchButton = document.querySelector('#search-button');
-let selectedResults = []
+const cityDropdown = document.querySelector("#city-dropdown");
+const typeDropdown = document.querySelector("#type-dropdown");
+const breedDropdown = document.querySelector("#breed-dropdown");
+const genderDropdown = document.querySelector("#gender-dropdown");
+const ageRangeDropdown = document.querySelector("#age-range-dropdown");
+const agencyDropdown = document.querySelector("#agency-dropdown");
+const searchButton = document.querySelector(".search-text");
+let selectedResults = [];
 
 
-searchButton.addEventListener('click', () => {
-    e.preventDefault();
-    const selectedCity = cityDropdown.value;
-    const selectedType = typeDropdown.value;
-    const selectedBreed = breedDropdown.value;
-    const selectedGender = genderDropdown.value;
-    const selectedAgeRange = ageRangeDropdown.value;
-    const selectedAgency = agencyDropdown.value;
 
-    let selectedResults ={
-        city: selectedCity,
-        type: selectedType,
-        breed: selectedBreed,
-        gender: selectedGender,
-        ageRange: selectedAgeRange,
-        agency: selectedAgency
-    };
+searchButton.addEventListener("click", () => {
+  const selectedCity = cityDropdown && cityDropdown.value ? [cityDropdown.value] : [];
+  const selectedType = typeDropdown && typeDropdown.value ? [typeDropdown.value] : [];
+  const selectedBreed = breedDropdown && breedDropdown.value ? [breedDropdown.value] : [];
+  const selectedGender = genderDropdown && genderDropdown.value ? [genderDropdown.value] : [];
+  const selectedAgeRange = ageRangeDropdown && ageRangeDropdown.value ? [ageRangeDropdown.value] : [];
+  const selectedAgency = agencyDropdown && agencyDropdown.value ? [agencyDropdown.value] : [];
 
-    fetch('/homepage', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(selectedResults)
-        })
-       .then(response => response.json())
-           .then(data => {
-         searchResultsContainer.innerHTML = html;
-         console.log(data);
-       })
-       .catch(error => {
-           console.error('Error:', error);
-      });
+
+  let selectedResults = {
+    city: selectedCity,
+    type: selectedType,
+    breed: selectedBreed,
+    gender: selectedGender,
+    ageRange: selectedAgeRange,
+    agency: selectedAgency,
+  };
+
+  const queryParams = []
+
+  if (selectedCity) {
+    queryParams.push(`city=${selectedCity}`);
+  }
+  if (selectedType) {
+    queryParams.push(`type=${selectedType}`);
+  }
+  if (selectedBreed) {
+    queryParams.push(`breed=${selectedBreed}`);
+    console.log(selectedBreed);
+  }
+  if (selectedGender) {
+    queryParams.push(`gender=${selectedGender}`);
+  }
+  if (selectedAgeRange) {
+    queryParams.push(`ageRange=${selectedAgeRange}`);
+  }
+  if (selectedAgency) {
+    queryParams.push(`agency=${selectedAgency}`);
+  }
+
+  
+  //hacer navegacion al query params a la query view
+  fetch("/query_results", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(selectedResults),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const queryHandlebar = `http://localhost:3001/query_results.handlebars?${queryParams.join('&')}`;
+
+      window.location.href = queryHandlebar;
+
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
-
-
+});
