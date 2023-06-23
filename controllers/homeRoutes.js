7da7e70f8854452ca078d75317f554f641120eff
Lizzz <alejandroraidi@gmail.com>
2023-06-22 20:37:09 -0600

    user login route and some homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,16 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+router.get('/login', async(req, res) => {
+    res.render('login');
+})
+
+router.get('/signup', async(req, res) => {
+    res.render('signup');
+})
+
+router.get('/dashboard', async(req, res) => {
+    res.render('dashboard');
+})
+
 module.exports = router;
