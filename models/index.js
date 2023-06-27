1cb2f703771c15f542ad73becd80feced0108a6c
Andrea Ceron <118624352+aceron2205@users.noreply.github.com>
2023-06-26 21:45:30 -0500

    added foreign key to Pet and Agency

--- models/index.js
+++ models/index.js
@@ -4,21 +4,18 @@ const Pet = require("./Pet");
 const User = require("./User");
 
 // Pets belong to Agency
-Agency.hasMany(Pet);
-Pet.belongsTo(Agency);
+Pet.belongsTo(Agency, { foreignKey: "agency_id" });
+Agency.hasMany(Pet, { foreignKey: "agency_id" });
 
 // Categories have many Products
 User.hasMany(Pet, {
   foreignKey: "favorite_pet_id",
 });
 
-// // Products belongToMany Tags (through ProductTag)
-// Pet.belongsto(Agency, {
-//   foreignKey: 'location',
-// });
 
 module.exports = {
   Pet,
   User,
   Agency,
 };
+// que cuando se haga el fetch como incluir lo de agency_id para jalar location
